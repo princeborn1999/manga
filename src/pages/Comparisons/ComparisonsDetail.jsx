18040efc274e7ed90cf314d127fa0f91745e6156
Prince <princeborn1999@gmail.com>
2023-10-07 21:21:28 +0800

    updates

--- src/pages/Comparisons/ComparisonsDetail.jsx
+++ src/pages/Comparisons/ComparisonsDetail.jsx
@@ -1,5 +1,12 @@
 function ComparisonsDetail() {
-  return <div>asdsad</div>;
+  return (
+    <div className="p-5">
+      <p>Shadow</p>
+      <p>Rough draft</p>
+      <p>Crowd</p>
+      <p>Background</p>
+    </div>
+  );
 }
 
 export default ComparisonsDetail;
